6d00ef0588679f74e471065059922f146ce333de
NievesGoCa <niieves99@gmail.com>
2026-07-01 19:24:40 +0200

    changes of desing and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
-import Education from './components/Education';
 import Contact from './components/Contact';
 import './styles/App.scss';
 
@@ -23,7 +22,7 @@ function App() {
         <About />
         <Experience />
         <Skills />
-        <Education />
+
         <Contact />
       </main>
 
